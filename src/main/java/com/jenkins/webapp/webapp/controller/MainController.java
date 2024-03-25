3d55ab46736b09ad61f92b0cf08f62c43c47d130
lowdisk17 <ra.andasan@gmail.com>
2024-03-25 15:44:57 +0800

    Update MainController.java

--- src/main/java/com/jenkins/webapp/webapp/controller/MainController.java
+++ src/main/java/com/jenkins/webapp/webapp/controller/MainController.java
@@ -2,15 +2,14 @@ package com.jenkins.webapp.webapp.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class MainController {
 
   @GetMapping("service")
-  public String getMethodName(@RequestParam String param) {
-      return new String();
+  public String getMethodName() {
+      return "Hello World";
   }
   
 }
